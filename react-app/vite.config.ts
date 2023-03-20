b071241d63ad180d8f3df0b69f4453b25db5f3d5
Irina <irenserge@mail.ru>
2023-03-20 20:48:40 +0300

    fix: fix bugs

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 import sassDts from "vite-plugin-sass-dts";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/setupTests.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
